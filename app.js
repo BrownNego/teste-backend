66bc43ac855e862a34d8c92136f6045f3fe8afa7
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2021-08-31 21:49:19 -0300

    Bootstrap

--- app.js
+++ app.js
@@ -10,21 +10,6 @@ const app = express();
 const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'abc123'))
 const session = driver.session()
 
-
-// app.get("/teste", function (req, res) {
-//     session
-//         .run('use crud CREATE (a:Person {name: "Alex"}) RETURN a')
-//         .then(function (result) {
-//             result.records.forEach(function (record) {
-//                 console.log(record._fields[0].properties);
-//             });
-//         })
-//         .catch(function (err) {
-//             console.log(err);
-//         });
-//     res.send('Ok!')
-// })
-
 //Template engine
 app.engine("handlebars", handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -42,45 +27,120 @@ app.get("/inserir", function (req, res) {
 });
 app.post("/controllerForm", urlencodeParser, function (req, res) {
     session
-        .run('use crud CREATE (a:Cliente {name: $name, email: $email, sexo: $sexo }) RETURN a',
+        .run(`use crud 
+                CREATE 
+                    (a:Cliente {Nome: $nome, Email: $email, Sexo: $sexo })                 
+                RETURN a`,
             {
-                name: req.body.name,
-                email: req.body.email,
-                // dtaNascimento: req.body.dtaNascimento,
-                sexo: req.body.sexo
+                nome: req.body.Nome,
+                email: req.body.Email,
+                sexo: req.body.Sexo
             })
+
         .then(function (result) {
             result.records.forEach(function (record) {
                 // console.log(record._fields[0].properties);
             });
 
-            res.render('controllerForm', { name: req.body.name })
+            // session
+            //     .run(`use crud 
+            //             MATCH
+            //                 (c:Cliente {Sexo: 'M'}),
+            //                 (s:Sexo {Tipo: 'M'})
+            //             MERGE (c)-[r:sexo]-(s)
+            //             RETURN r`)
+
+            //     .then(function (result2) {
+            //         res.render('controllerForm', { name: req.body.Nome })
+
+            //     })
+            //     .catch(function (err) {
+            //         console.log(err);
+            //     });
+
+            // session
+            //     .run(`use crud 
+            //             MATCH
+            //                 (c:Cliente {Sexo: 'F'}),
+            //                 (s:Sexo {Tipo: 'F'})
+            //             MERGE (c)-[r:sexo]-(s)
+            //             RETURN r`)
+
+            //     .then(function (result3) {
+            //         res.render('controllerForm', { name: req.body.Nome })
+
+            //     })
+            //     .catch(function (err) {
+            //         console.log(err);
+            //     });
+
+            res.render('controllerForm', { name: req.body.Nome })
+
         })
         .catch(function (err) {
             console.log(err);
         });
-
+    res.redirect('/select');
 });
 
 //SELECT
-app.get("/select:id?", function (req, res) {
+app.get("/select/:id?", function (req, res) {
 
     if (!req.params.id) {
         session
-            .run('use crud MATCH (n) RETURN n LIMIT 25')
+            .run(`use crud 
+                    MATCH (n:Cliente) 
+                    RETURN n LIMIT 25`)
+
             .then(function (result) {
                 var clientes = [];
                 result.records.forEach(function (record) {
                     clientes.push({
-                        name: record._fields[0].properties.name,
-                        sexo: record._fields[0].properties.sexo,
-                        email: record._fields[0].properties.email
+                        id: record._fields[0].identity.low,
+                        nome: record._fields[0].properties.Nome,
+                        sexo: record._fields[0].properties.Sexo,
+                        email: record._fields[0].properties.Email
                     });
 
-                    console.log(record._fields[0].properties);
+                    // console.log(record._fields[0].identity.low);
+                });
+                res.render('select', {
+
+                    clientes: clientes
+                });
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
+    } else {
+        session
+            .run(`use crud 
+                    MATCH (n:Cliente) 
+                    WHERE id(n) = $id 
+                    RETURN n`,
+                {
+                    id: req.body.id
+
+                })
+
+            .then(function (result) {
+                var clientes = [];
+                result.records.forEach(function (record) {
+                    clientes.push({
+                        id: record._fields[0].identity.low,
+                        nome: record._fields[0].properties.Nome,
+                        sexo: record._fields[0].properties.Sexo,
+                        email: record._fields[0].properties.Email
+                    });
+
+                    // console.log(record._fields[0].properties);
                 });
 
+                console.log(clientes)
+
                 res.render('select', {
+
                     clientes: clientes
                 });
             })
